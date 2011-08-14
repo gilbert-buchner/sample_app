2db6a5eb13157e86c878cebb97f7341cf144f44b
Gilbert Buchner <gilbert.buchner@gmx.at>
2011-08-14 19:47:07 +0200

    Finished layout and routes

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -6,15 +6,19 @@ describe "LayoutLinks" do
       response.should have_selector('title', :content => "Home")
   end
   it "should have a Contact page at '/'" do
-      get '/'
+      get '/contact'
       response.should have_selector('title', :content => "Contact")
   end
   it "should have a About page at '/'" do
-      get '/'
+      get '/about'
       response.should have_selector('title', :content => "About")
   end
   it "should have a Help page at '/'" do
-      get '/'
+      get '/help'
       response.should have_selector('title', :content => "Help")
   end
+  it "should have a signup page at '/signup'" do
+      get '/signup'
+      response.should have_selector("title", :content => "Sign up")
+    end
 end
